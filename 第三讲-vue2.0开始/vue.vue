5e0cd99b0a8bf4306bff6255eb135b53d1559722
huyiwill <huyiwill@163.com>
2018-06-05 18:35:44 +0800

    vue-父子组件以及路由功能

---
+++ 第三讲-vue2.0开始/vue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="width: 100px;height: 100px;background: aqua;"></div>
+</template>
+
+<script>
+  export default {
+    name: "vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
